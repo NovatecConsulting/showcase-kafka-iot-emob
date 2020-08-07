ecbb56c601d968cf4ef314614977ec264685af99
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-08-07 08:39:03 +0200

    Configure Kafka in cluster mode.

--- test-connect.sh
+++ test-connect.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 for (( c=1; c<=10; c++ ))
 do
     docker run -it --rm --name mqtt-publisher --network showcase_emob efrecon/mqtt-client pub -h hivemq -t "test" -m "{\"id\":$c,\"message\":\"This is a test $c\"}"
