ad73c5f145b0ffbd971b28879da2ad0ef4668dde
nadjahagen <Nadja.Hagen@novatec-gmbh.de>
2020-08-21 08:36:18 +0000

    Create tables and compacted topic

--- setup-ksqlquery.sh
+++ setup-ksqlquery.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-curl -d @"./config/ksql-config/ksqlquery.json" -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" -X POST "http://localhost:8088/ksql" 
-
-
+curl -d @"./config/ksql-config/ksqlConvertRawData.json" -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" -X POST "http://localhost:8088/ksql" 
+curl -d @"./config/ksql-config/ksqlCompactTopic.json" -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" -X POST "http://localhost:8088/ksql"  
 
