fc09d0360d3a0c0f6f65a5b05495c3735698136f
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-09-01 10:25:50 +0200

    - Add client_ID to avro value-schema of all Kafka topics. - Replace substring operator in KSQL query with regex substring extract operator.

--- config/mongodb-config/mongo_setup.sh
+++ config/mongodb-config/mongo_setup.sh
@@ -28,7 +28,7 @@ mongo --host mongodb://mongo:27017/?replicaSet=rs0 <<EOF
   db.createCollection("StationLocations");
   db.StationLocations.insertMany([
     {CLIENT_ID:"CIQ000000017",x:25.1,y:25.2},
-    {CLIENT_ID:"CIQ000000018",x:35.1,y:35.2},
-    {CLIENT_ID:"CIQ000000019",x:45.1,y:45.2}
+    {CLIENT_ID:"CIQ000000001",x:35.1,y:35.2},
+    {CLIENT_ID:"CIQ000000012",x:45.1,y:45.2}
   ])
 EOF
