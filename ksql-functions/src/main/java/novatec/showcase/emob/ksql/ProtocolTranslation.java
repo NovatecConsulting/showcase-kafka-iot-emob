5ecb62ea286405d79847e7dcd8227899f4c37ccc
Hieu Vo <duyhieu.vo@notatec-gmbh.de>
2020-09-18 11:56:19 +0200

    Map messages of new protocol to version 0.8 Update README

--- ksql-functions/src/main/java/novatec/showcase/emob/ksql/ProtocolTranslation.java
+++ ksql-functions/src/main/java/novatec/showcase/emob/ksql/ProtocolTranslation.java
@@ -3,6 +3,7 @@ package novatec.showcase.emob.ksql;
 import io.confluent.ksql.function.udf.Udf;
 import io.confluent.ksql.function.udf.UdfDescription;
 import io.confluent.ksql.function.udf.UdfParameter;
+import org.apache.commons.lang3.StringUtils;
 
 @UdfDescription(name = "protocol_translation", description = "Translate new messages to protocol 0.8")
 public class ProtocolTranslation {
@@ -12,15 +13,15 @@ public class ProtocolTranslation {
       @UdfParameter(value = "STATUS", description = "Charging status") final String status,
       @UdfParameter(value = "EVENT", description = "Charging event") final String event,
       @UdfParameter(value = "ACTION", description = "Charging action") final String action,
-      @UdfParameter(value = "AMOUNT", description = "Charging amount") final long amount,
-      @UdfParameter(value = "MAX", description = "Charging max") final long max,
-      @UdfParameter(value = "RESPONSE_AMOUNT", description = "Charging response amount") final long response_amount,
-      @UdfParameter(value = "RESPONSE_MAX", description = "Charging response max") final long response_max) {
-      //if (status.equals("ready") || event.equals("EV_lost")) return "ready";
-      //if (event.equals("ev"))  return "ev";
-      //if (event.equals("start")) return "charging 0/" + max;
-      //if (event.equals("stop")) return "charged " + amount + "/" + max;
-      //if (action.equals("amount")) return "charging " + response_amount + "/" + response_max;
+      @UdfParameter(value = "AMOUNT", description = "Charging amount") final Long amount,
+      @UdfParameter(value = "MAX", description = "Charging max") final Long max,
+      @UdfParameter(value = "RESPONSE_AMOUNT", description = "Charging response amount") final Long response_amount,
+      @UdfParameter(value = "RESPONSE_MAX", description = "Charging response max") final Long response_max) {
+      if (StringUtils.equals(status,"ready") || StringUtils.equals(event,"EV_lost")) return "ready";
+      if (StringUtils.equals(event,"ev"))  return "ev";
+      if (StringUtils.equals(event,"start")) return "charging 0/" + max;
+      if (StringUtils.equals(event,"stop")) return "charged " + amount + "/" + max;
+      if (StringUtils.equals(action,"amount")) return "charging " + response_amount + "/" + response_max;
       return "ready";
     }
 }
---
+++ ksql-functions/src/test/java/novatec/showcase/emob/ksql/ProtocolTranslationTest.java
@@ -0,0 +1,22 @@
+package novatec.showcase.emob.ksql;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class ProtocolTranslationTest{ 
+    @Test
+    public void protocolTranslation(){
+        ProtocolTranslation translator = new ProtocolTranslation();
+        String charge = translator.protocol_translation(null,null,null,null,null,null,null);
+        assertEquals(charge,"ready");
+        charge = translator.protocol_translation("ready",null,null,null,null,null,null);
+        assertEquals(charge,"ready");
+        charge = translator.protocol_translation(null,"ev",null,null,null,null,null);
+        assertEquals(charge,"ev");
+        charge = translator.protocol_translation(null,"start",null,null,40L,null,null);
+        assertEquals(charge,"charging 0/40");
+        charge = translator.protocol_translation(null,"stop",null,30L,40L,null,null);
+        assertEquals(charge,"charged 30/40");
+    }
+}
