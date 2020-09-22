b4410693bdc0b89a637815755757868fa83b708b
nh <Nadja.Hagen@novatec-gmbh.de>
2020-09-22 09:02:38 +0200

    Fixed bug which occured when starting the test driver

--- config/mongodb-config/mongo_setup.sh
+++ config/mongodb-config/mongo_setup.sh
@@ -26,23 +26,23 @@ mongo --host mongodb://mongo:27017/?replicaSet=rs0 <<EOF
   db.createCollection("ChargingStations");
   db.createCollection("StationLocations");
   db.StationLocations.insertMany([
-    {CLIENT_ID:"CIQ100000001",lat:48.747,long:9.164},
-    {CLIENT_ID:"CIQ100000002",lat:49.244,long:7.373},
-    {CLIENT_ID:"CIQ100000003",lat:52.570,long:13.409},
-    {CLIENT_ID:"CIQ100000004",lat:48.193,long:11.549},
-    {CLIENT_ID:"CIQ100000005",lat:53.617,long:9.987},
-    {CLIENT_ID:"CIQ100000006",lat:37.400,long:-3.609},
-    {CLIENT_ID:"CIQ100000007",lat:48.684,long:9.168},
-    {CLIENT_ID:"CIQ100000008",lat:50.118,long:8.647},
-    {CLIENT_ID:"CIQ100000009",lat:51.317,long:12.344},
-    {CLIENT_ID:"CIQ100000010",lat:52.425,long:9.517},
-    {CLIENT_ID:"CIQ100000011",lat:53.088,long:9.799},
-    {CLIENT_ID:"CIQ100000012",lat:47.677,long:9.174},
-    {CLIENT_ID:"CIQ100000013",lat:48.682,long:9.161},
-    {CLIENT_ID:"CIQ100000014",lat:49.038,long:8.418},
-    {CLIENT_ID:"CIQ100000015",lat:51.057,long:13.764},
-    {CLIENT_ID:"CIQ100000016",lat:49.456,long:11.104},
-    {CLIENT_ID:"CIQ100000017",lat:51.009,long:6.954},
-    {CLIENT_ID:"CIQ100000018",lat:48.520,long:9.060}
+    {CLIENT_ID:"CIQ100000001",latitude:48.747,longitude:9.164},
+    {CLIENT_ID:"CIQ100000002",latitude:49.244,longitude:7.373},
+    {CLIENT_ID:"CIQ100000003",latitude:52.570,longitude:13.409},
+    {CLIENT_ID:"CIQ100000004",latitude:48.193,longitude:11.549},
+    {CLIENT_ID:"CIQ100000005",latitude:53.617,longitude:9.987},
+    {CLIENT_ID:"CIQ100000006",latitude:37.400,longitude:-3.609},
+    {CLIENT_ID:"CIQ100000007",latitude:48.684,longitude:9.168},
+    {CLIENT_ID:"CIQ100000008",latitude:50.118,longitude:8.647},
+    {CLIENT_ID:"CIQ100000009",latitude:51.317,longitude:12.344},
+    {CLIENT_ID:"CIQ100000010",latitude:52.425,longitude:9.517},
+    {CLIENT_ID:"CIQ100000011",latitude:53.088,longitude:9.799},
+    {CLIENT_ID:"CIQ100000012",latitude:47.677,longitude:9.174},
+    {CLIENT_ID:"CIQ100000013",latitude:48.682,longitude:9.161},
+    {CLIENT_ID:"CIQ100000014",latitude:49.038,longitude:8.418},
+    {CLIENT_ID:"CIQ100000015",latitude:51.057,longitude:13.764},
+    {CLIENT_ID:"CIQ100000016",latitude:49.456,longitude:11.104},
+    {CLIENT_ID:"CIQ100000017",latitude:51.009,longitude:6.954},
+    {CLIENT_ID:"CIQ100000018",latitude:48.520,longitude:9.060}
   ])
 EOF
