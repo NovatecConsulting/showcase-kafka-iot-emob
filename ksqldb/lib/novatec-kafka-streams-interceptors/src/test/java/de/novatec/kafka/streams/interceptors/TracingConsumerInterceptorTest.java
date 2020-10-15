832145b61bf5c8231582eda4bd83f6dc6b75f8bb
Uwe Eisele <code@uweeisele.eu>
2020-10-15 23:31:56 +0200

    ksqldb interceptors, vscode devcontainer support and Java 11 - added ksqldb interceptors which attach breadcrumbs to events to retrace the event flow - added vscode devcontainer support to ksqldb and connect libs - migrate ksqldb libs to Java 11 Hint: run './emob-dc.sh dc build' to rebuild all containers, because libs has been added

---
+++ ksqldb/lib/novatec-kafka-streams-interceptors/src/test/java/de/novatec/kafka/streams/interceptors/TracingConsumerInterceptorTest.java
@@ -0,0 +1,23 @@
+package de.novatec.kafka.streams.interceptors;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TracingConsumerInterceptorTest
+{
+    public TracingConsumerInterceptorTest() {
+    }
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void testMore()
+    {
+        assertTrue( true );
+    }
+}
